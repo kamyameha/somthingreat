362a7719c1cd1139f7301e9903a08fe7e9b6dafd
kamyameha <grascam@gmail.com>
2026-06-15 22:05:22 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,9 @@
-const CACHE_NAME = 'calisthenics-pwa-v9';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './supabase-config.js',
-  './session-repair.js?v=1',
-  './manifest.json',
-  './apple-touch-icon.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-maskable-512.png'
-];
+self.addEventListener("install", () => self.skipWaiting());
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      ))
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
       .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
