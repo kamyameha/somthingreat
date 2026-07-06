d95e70947798010d19f27d4c76224d7f1dc56518
kamyameha <grascam@gmail.com>
2026-07-06 13:13:23 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const INITIAL_AUTH_SEARCH = window.location.search || '';
 const INITIAL_AUTH_HASH = window.location.hash || '';
-const APP_VERSION = 'v8-84-submenu-normal-flow';
+const APP_VERSION = 'v8-85-submenu-menu-shape';
 const SUPABASE_READY = Boolean(
   window.supabase &&
   window.SUPABASE_URL &&
