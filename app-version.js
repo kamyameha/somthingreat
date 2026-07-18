43076b595f60ea6b8c865e06bb30f58701ac0614
Kamyameha <grascam@gmail.com>
2026-07-18 17:30:59 +0300

    Bump version for native activity select and modal fixes

--- app-version.js
+++ app-version.js
@@ -1,5 +1,5 @@
 (function (scope) {
-  const APP_VERSION = '2026.07.18.172000';
+  const APP_VERSION = '2026.07.18.173500';
 
   scope.APP_VERSION = APP_VERSION;
   scope.SOMTHINGREAT_VERSION = APP_VERSION;
