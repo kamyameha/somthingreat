a02592c4b7fd16632663fb7404578cfbc64a04fb
Kamyameha <kamyameha@MacBook-Pro.local>
2026-07-23 14:10:44 +0300

    Fix iOS mascot viewport clipping

--- app-version.js
+++ app-version.js
@@ -1,5 +1,5 @@
 (function (scope) {
-  const APP_VERSION = '2026.07.23.110410';
+  const APP_VERSION = '2026.07.23.111038';
 
   scope.APP_VERSION = APP_VERSION;
   scope.SOMTHINGREAT_VERSION = APP_VERSION;
