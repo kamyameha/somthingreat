8a58a749867b18578a32c867409dc0f8a2cd7f48
Kamyameha <kamyameha@MacBook-Pro.local>
2026-07-24 15:44:31 +0300

    Separate workout safe area from first tile

--- app-version.js
+++ app-version.js
@@ -1,5 +1,5 @@
 (function (scope) {
-  const APP_VERSION = '2026.07.24.123452';
+  const APP_VERSION = '2026.07.24.124426';
 
   scope.APP_VERSION = APP_VERSION;
   scope.SOMTHINGREAT_VERSION = APP_VERSION;
