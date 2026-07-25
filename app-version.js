ee248927cebc1aaf8088daac2f51c1b720eedbb0
Kamyameha <kamyameha@MacBook-Pro.local>
2026-07-25 19:12:35 +0300

    Restore Today navbar position

--- app-version.js
+++ app-version.js
@@ -1,5 +1,5 @@
 (function (scope) {
-  const APP_VERSION = '2026.07.25.154412';
+  const APP_VERSION = '2026.07.25.161223';
 
   scope.APP_VERSION = APP_VERSION;
   scope.SOMTHINGREAT_VERSION = APP_VERSION;
